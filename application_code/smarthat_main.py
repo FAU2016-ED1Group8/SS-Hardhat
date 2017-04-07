c1ec89c5f8f1073d47b1cf0ab3ff5a089dbe3615
fpsoares <fprofeta81@gmail.com>
2017-04-07 01:59:07 -0400

    moved all main code to bottom

--- application_code/smarthat_main.py
+++ application_code/smarthat_main.py
@@ -11,53 +11,6 @@ import datetime
 from pyrebase import pyrebase
 from subprocess import call
 
-#    Use the Broadcom SOC Pin numbers
-#    Setup the Pin with Internal pullups enabled and PIN in reading mode.
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(5, GPIO.IN)  #   log hazard
-GPIO.setup(6, GPIO.IN)  #   start end call
-GPIO.setup(13, GPIO.IN)  #   camera
-GPIO.setup(26, GPIO.IN)  #   shutdown
-GPIO.setup(11, GPIO.OUT)  #   laser
-GPIO.setup(9, GPIO.IN)  #   mag sensor ADC1
-GPIO.setup(10, GPIO.IN)  #   mag sensor ADC2
-
-ser = serial.Serial("/dev/ttyS0",115200,timeout=3)  #   FONA Serial
-gpio.setup(callbutton,gpio.IN)  #   input
-
-callstate = False
-
-config = {
-  "apiKey": "apiKey",
-  "authDomain": "smartsafetyhardhat.firebaseapp.com",
-  "databaseURL": "https://smartsafetyhardhat.firebaseio.com",
-  "storageBucket": "smartsafetyhardhat.appspot.com"
-}
-
-firebase = pyrebase.initialize_app(config)
-db = firebase.database()
-
-#    time variable
-now = datetime.datetime.now()
-time = now.strftime('%A %B %d, %Y %I:%M:%S %p')
-#  time = str(datetime.datetime.now())
-
-
-
-#    Record Audio
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)
-
-
-while 1:
-    GPIO.add_event_detect(5, GPIO.FALLING, callback = logHazard, bouncetime = 2000)         #  board 29
-    GPIO.add_event_detect(6, GPIO.FALLING, callback = startEndCall, bouncetime = 2000)      #  board 31
-    GPIO.add_event_detect(13, GPIO.FALLING, callback = capturePicture, bouncetime = 2000)   #  board 33
-    GPIO.add_event_detect(26, GPIO.FALLING, callback = Shutdown, bouncetime = 2000)         #  board 37
-    GPIO.add_event_detect(9, GPIO.FALLING, callback = checkMagField, bouncetime = 2000)         #  board 21
-    #  time.sleep(1)
 
 #   Our function on what to do when the button is pressed
 def Shutdown():
@@ -130,3 +83,51 @@ def startAnswerCall():
 #   Add our function to execute when the button pressed event happens / function called on button down
 
 #   Now wait!
+
+#    Use the Broadcom SOC Pin numbers
+#    Setup the Pin with Internal pullups enabled and PIN in reading mode.
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(5, GPIO.IN)  #   log hazard
+GPIO.setup(6, GPIO.IN)  #   start end call
+GPIO.setup(13, GPIO.IN)  #   camera
+GPIO.setup(26, GPIO.IN)  #   shutdown
+GPIO.setup(11, GPIO.OUT)  #   laser
+GPIO.setup(9, GPIO.IN)  #   mag sensor ADC1
+GPIO.setup(10, GPIO.IN)  #   mag sensor ADC2
+
+ser = serial.Serial("/dev/ttyS0",115200,timeout=3)  #   FONA Serial
+gpio.setup(callbutton,gpio.IN)  #   input
+
+callstate = False
+
+config = {
+  "apiKey": "apiKey",
+  "authDomain": "smartsafetyhardhat.firebaseapp.com",
+  "databaseURL": "https://smartsafetyhardhat.firebaseio.com",
+  "storageBucket": "smartsafetyhardhat.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+#    time variable
+now = datetime.datetime.now()
+time = now.strftime('%A %B %d, %Y %I:%M:%S %p')
+#  time = str(datetime.datetime.now())
+
+
+
+#    Record Audio
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+
+while 1:
+    GPIO.add_event_detect(5, GPIO.FALLING, callback = logHazard, bouncetime = 2000)         #  board 29
+    GPIO.add_event_detect(6, GPIO.FALLING, callback = startEndCall, bouncetime = 2000)      #  board 31
+    GPIO.add_event_detect(13, GPIO.FALLING, callback = capturePicture, bouncetime = 2000)   #  board 33
+    GPIO.add_event_detect(26, GPIO.FALLING, callback = Shutdown, bouncetime = 2000)         #  board 37
+    GPIO.add_event_detect(9, GPIO.FALLING, callback = checkMagField, bouncetime = 2000)         #  board 21
+    #  time.sleep(1)
