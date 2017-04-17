01c25d8ee72871e488470803249fc9192ba9f37a
fpsoares <fprofeta81@gmail.com>
2017-04-17 01:19:53 -0400

    Fixing interrupt logic

--- application_code/interrup_watcher.py
+++ application_code/interrup_watcher.py
@@ -22,5 +22,33 @@ pin_buzzer = 26
 GPIO.setup([btn_camera, btn_phone, btn_loghaz, btn_shutdown, pin_magsensor], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup([pin_laser, pin_warnleds, pin_buzzer], GPIO.OUT, initial.GPIO.LOW)
 
-def camera():
-    call(["python3 capture_image.py"])
+def cap_image():
+    # call(["python3 capture_image.py"])
+    print("Camera")
+
+def phone_call():
+    print("Phone")
+
+def hazard_log():
+    print("Hazard")
+
+def handle(pin):
+    t = None
+    if pin == btn_camera:
+        cap_image()
+    else if pin == btn_phone:
+        phone_call()
+    else if pin == btn_loghaz:
+        hazard_log()
+
+
+
+GPIO.add_event_detect(btn_camera, GPIO.FALLING, handle)
+GPIO.add_event_detect(btn_camera, GPIO.FALLING, handle)
+GPIO.add_event_detect(btn_camera, GPIO.FALLING, handle)
+
+
+
+
+while True:
+	time.sleep(1e6)
