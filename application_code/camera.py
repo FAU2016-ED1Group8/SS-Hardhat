a28006f9f57f98c501ade25f75b0c1f9d4473032
gcasinillo <gcasinillo2015@gmail.com>
2017-04-22 13:03:30 -0400

    Add files via upload

---
+++ application_code/camera.py
@@ -0,0 +1,56 @@
+import os
+import datetime
+from datetime import datetime as dt
+
+import speech_recognition as sr
+import datetime                         #time library
+from pyrebase import pyrebase           #pyrebase library
+
+#sets up firebase
+config = {
+  "apiKey": "apiKey",
+  "authDomain": "smartsafetyhardhat.firebaseapp.com",
+  "databaseURL": "https://smartsafetyhardhat.firebaseio.com",
+  "storageBucket": "smartsafetyhardhat.appspot.com"
+}
+
+#call(["fswebcam" -r 640x480 --save image4.jpg])
+#print("image4 captured")
+firebase = pyrebase.initialize_app(config)
+
+now = dt.today().strftime("%Y%m%d%H%M%S")
+file_name = now + '.jpg'
+
+os.system('fswebcam -r 640x480 -S 3 --jpeg 50 --save /home/pi/Desktop/images/'+ file_name)
+
+
+#####################################################################
+#####################Write to Storage################################
+#####################################################################
+
+
+print(file_name)
+storage = firebase.storage()
+#storage.child("Camera/"+"forest").put("/home/pi/Desktop/images/forest.jpg")
+storage.child("Camera/"+file_name).put("/home/pi/Desktop/images/"+file_name)
+
+
+#####################################################################
+####################Write to Database################################
+#####################################################################
+db = firebase.database()
+
+#time variable
+now = datetime.datetime.now()
+time = now.strftime('%A %B %d, %Y %I:%M:%S %p')
+token = None
+
+imageUrl = storage.child("Camera/"+file_name).get_url(token)
+
+#variable that will be written to the database
+data = {"ImageName":imageUrl, "date": time}                 
+db.child("Camera Data").push(data)
+
+
+print("success!")
+
