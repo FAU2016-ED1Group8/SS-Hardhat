b889651aa9ddd16010378283ca4685b77e86089b
fpsoares <fprofeta81@gmail.com>
2017-04-28 22:09:37 -0400

    Added voice dialing function

--- application_code/interrupt_watcher.py
+++ application_code/interrupt_watcher.py
@@ -2,13 +2,15 @@ import RPi.GPIO as GPIO
 import time
 import os
 import speech_recognition as sr
-import datetime
+import datetime import datetime as dt
 import serial
 from pyrebase import pyrebase
 from subprocess import call
 import geopy                            #geopy for coordinate comparison
 from geopy.distance import vincenty     #geopy function for long/lat comparison
 from geopy.geocoders import Nominatim
+from fuzzywuzzy import fuzz             #string compare module
+from fuzzywuzzy import process
 
 config = {
   "apiKey": "apiKey",
@@ -105,6 +107,70 @@ def cap_image():
 
 def phone_call():
     print("Phone")
+    inputnum = voice_dial()
+    #inputnum=str('5618438458')
+    # sercommand = "ATD"+str(inputnum)+";\r\n"
+    # ser.write(sercommand.encode())
+    # phone_call()
+    # time.sleep(20)
+    # ser.write("ATH".encode())
+    print('Calling now')
+
+def record_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+
+    #Speech recognition using Google Speech Recognition
+    try:
+        # for testing purposes, we're just using the default API key
+        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+        # instead of `r.recognize_google(audio)`
+        # print("You said: " + r.recognize_google(audio))
+        google_translate = r.recognize_google(audio)
+        return google_translate
+    except sr.UnknownValueError:
+        return "Google Speech Recognition could not understand audio"
+    except sr.RequestError as e:
+        return "Could not request results from Google Speech Recognition service; {0}".format(e)
+
+def voice_dial():
+    #retrieves number of contacts from firebase
+    num_of_contacts = list(range(len([users.key() for users in (db.child("Contact Information").get()).each()])))
+
+
+    #gets the first name/last name/number of each person in the contacts from firebase
+    first_name = [(db.child("Contact Information").child(str(x)).child("First").get()).val() for x in num_of_contacts ]
+    last_name = [(db.child("Contact Information").child(str(x)).child("Last").get()).val() for x in num_of_contacts ]
+    phone_num = [(db.child("Contact Information").child(str(x)).child("Number").get()).val() for x in num_of_contacts ]
+
+    ## Send Beep ## TODO
+
+    call_string = str(record_audio())
+
+    #variable that holds the first/last names of the people in the contact list
+    k =  list (zip(first_name, last_name))
+
+    #joins the first/last names of the people in the contact list
+    complete_names = [(' '.join(j)) for j in k]
+
+    #removes "(", ")","-" from the phone numbers retrieved from firebase
+    sanitized_phone_numbers = [int(''.join(c for c in x if c.isdigit())) for x in phone_num]
+
+    #performs the string compare using fuzzywuzzy module
+    string_compare = process.extractOne(call_string, complete_names, scorer = fuzz.partial_ratio)
+
+    #variable that holds the name of the string match
+    name_match = str((string_compare)[0])
+
+    #phone and name directory of the contact list in firebase
+    phone_directory =  dict (zip(complete_names, sanitized_phone_numbers))
+
+    #variable that holds the number to be dialed
+    phone_string = str(phone_directory.get(name_match))
+
+    return(phone_string)
 
 def hazard_log():
     print("Hazard")
@@ -115,6 +181,7 @@ def hazard_log():
         print(gpsLatLon[3],gpsLatLon[4],gpsLatLon[2])
 
 def shutdown():
+    print("System is shutting down")
     os.system("sudo shutdown -h 'now'")
 
 def handle(pin):
@@ -124,20 +191,13 @@ def handle(pin):
         cap_image()
     elif pin == btn_phone:
         print("Phone Handle")
-        inputnum=str('5618438458')
-        sercommand = "ATD"+str(inputnum)+";\r\n"
-        ser.write(sercommand.encode())
         phone_call()
-        time.sleep(20)
-        ser.write("ATH".encode())
-        print('Calling now')
 
     elif pin == btn_loghaz:
         print("Haz Handle")
         hazard_log()
 
     elif pin == btn_shutdown:
-        print("System is shutting down")
         shutdown()
 
 
