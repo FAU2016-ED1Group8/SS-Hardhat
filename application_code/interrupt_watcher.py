92d7bd793f44528e39345f164b024237d5444a7b
fpsoares <fprofeta81@gmail.com>
2017-05-01 01:24:47 -0400

    Fixing syntax

--- application_code/interrupt_watcher.py
+++ application_code/interrupt_watcher.py
@@ -132,7 +132,7 @@ def start_call(inputnum):
     print("Starting call")
     callingVar = 'ATD'+str(inputnum)+';\r\n'
     ser.write(callingVar.encode())
-    print('Calling now: $d' % inputnum)
+    print('Calling now: %d' % inputnum)
 
 def end_call():
     print("ending call")
